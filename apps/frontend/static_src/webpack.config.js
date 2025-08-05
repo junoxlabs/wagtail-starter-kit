c155a0ae95d226b0b01ea8a28de0a12a755d7819
Hanz <haniel56@zoho.eu>
2025-08-05 15:55:57 +0530

    fix: tailwind v4 JIT compiler

--- apps/frontend/static_src/webpack.config.js
+++ apps/frontend/static_src/webpack.config.js
@@ -3,22 +3,20 @@ const path = require("path");
 const glob = require("glob");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const WebpackAssetsManifest = require("webpack-assets-manifest");
+const { WebpackAssetsManifest } = require("webpack-assets-manifest");
 
 const getEntryObject = () => {
   const entries = {};
   // for javascript/typescript entry file
-  glob
-    .sync(path.join(__dirname, "./src/app/*.{js,ts}"))
-    .forEach((filePath) => {
-      const name = path.basename(filePath);
-      const extension = path.extname(filePath);
-      const entryName = name.replace(extension, "");
-      if (entryName in entries) {
-        throw new Error(`Entry file conflict: ${entryName}`);
-      }
-      entries[entryName] = filePath;
-    });
+  glob.sync(path.join(__dirname, "./src/app/*.{js,ts}")).forEach((filePath) => {
+    const name = path.basename(filePath);
+    const extension = path.extname(filePath);
+    const entryName = name.replace(extension, "");
+    if (entryName in entries) {
+      throw new Error(`Entry file conflict: ${entryName}`);
+    }
+    entries[entryName] = filePath;
+  });
   return entries;
 };
 
@@ -26,8 +24,8 @@ module.exports = {
   entry: getEntryObject(),
   output: {
     path: path.resolve(__dirname, "../build"),
-    filename: "js/[name].[chunkhash:8].js",
-    chunkFilename: "js/[name].[chunkhash:8].chunk.js",
+    filename: "js/[chunkhash:8].js",
+    chunkFilename: "js/[id].[chunkhash:8].chunk.js",
     publicPath: "/static/",
   },
   optimization: {
@@ -40,8 +38,8 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: "css/[name].[contenthash].css",
-      chunkFilename: "css/[id].[contenthash].css",
+      filename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[id].[contenthash:8].css",
     }),
     new WebpackAssetsManifest({
       entrypoints: true,
